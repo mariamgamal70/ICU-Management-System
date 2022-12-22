5c05ac16eb5493415d70215e4b18d404b4a5c451
mariamgamal70 <mariamgamal70@gmail.com>
2022-12-23 00:24:35 +0200

    updated navbar

--- navbar.js
+++ navbar.js
@@ -1,7 +1,18 @@
-let navbar=document.querySelector('#bigsidenavbar');
+let menuicon=document.querySelector('#menuicon');
+let navbar=document.querySelector('#navbardiv');
+let content=document.querySelector('.navbarcontent');
+let close=document.querySelector('#xicon');
 function togglenavbar(){
-    if(navbar.style.display=='block')
-        navbar.style.display='none';
-    else
-    navbar.style.display='block';
+    if(navbar.style.width=='200px'){
+        navbar.style.width='0px';
+        content.style.marginLeft='0px';
+        menuicon.style.display='inline';
+        close.style.display='none';
+    }
+    else{
+    navbar.style.width='200px';
+    content.style.marginLeft='200px';
+    menuicon.style.display='none';
+    close.style.display='inline';
+}
 }
