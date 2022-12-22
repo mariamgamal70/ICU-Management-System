7054a5130c69552fa3f815d83957f7b20db2c47a
mariamgamal70 <mariamgamal70@gmail.com>
2022-12-22 22:05:41 +0200

    updated sign in page and added navbar

---
+++ navbar.js
@@ -0,0 +1,7 @@
+let navbar=document.querySelector('#bigsidenavbar');
+function togglenavbar(){
+    if(navbar.style.display=='block')
+        navbar.style.display='none';
+    else
+    navbar.style.display='block';
+}
