743bb919ca32f1ce30b40692d1eb2571c6473045
mariamgamal70 <mariamgamal70@gmail.com>
2022-12-26 23:24:54 +0200

    commit1
    -adjusted file structure
    -added functions for index page buttons
    -added separate css file for navbar
    -changed the way images and css files are linked to html in href and src

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return ("hello world")
+   return render_template('index.html')
+
+@app.route('/signin')
+def signin():
+   return render_template('signin.html')
+
+@app.route('/contactus')
+def contactus():
+   return render_template('contactus.html')
 
 if __name__ == '__main__':
    app.run()
