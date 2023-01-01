32336b489e121e6dcd6fb681295eb996a5bbad72
mariamgamal70 <mariamgamal70@gmail.com>
2023-01-01 03:59:23 +0200

    Summary in description
    -added email system for contact us
    -fixed sign in , just needs renaming when database is finished
    -added to get name on signing in on their hompage in nurse and receptionist

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request,session,flash
+from flask_session import Session
+from datetime import datetime
 import mysql.connector
+from flask_mail import Mail, Message
+
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"]=False
+app.config["SESSION_TYPE"]="filesystem"
+Session(app)
+
+app.config["MAIL_SERVER"]="smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USERNAME"] = "mariamgamal70@gmail.com" #senders email
+app.config["MAIL_PASSWORD"] = "azaesjimhgtnsydq" #senders password
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+mail = Mail()
+mail.init_app(app)
 mydb = mysql.connector.connect(
    host="localhost",
    user="root",
@@ -9,102 +26,143 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 #GET used when no info is sent(written in URL) , POST is used when info is sent(Ex:Sensitive info)(not written in URL)
-#get function route('route)
+#get function route("route)
 #functions are GET only by default, to make it GET and POST , u should define it as a parameter in route
-#route('route',methods=['POST','GET'])
+#route("route",methods=["POST","GET"])
 # in case it is GET and POST you should check inside the function whether the incoming is a get or post request , if its a get ,return template, if its a post , send data do changes
 
-@app.route('/')#GET METHOD
+
+def sendmessage(result):
+   msg = Message(subject="Inquiry/Complaint", sender=result['email'], recipients=["mariamgamal70@gmail.com"])
+   msg.body = 'from ' + result['email'] + '\n'+'Name: '+result['firstname'] +' '+result['lastname'] + '\n' 'Complaint: ' + result['complaint']
+   flash(msg.body)
+   mail.send(msg)
+
+
+@app.route("/")#GET METHOD
 def index():
-   return render_template('index.html')
+   return render_template("index.html")
 
 
 @app.route('/adminhome')
 def Adminhome():
    diction={"name":"sharif"}
-  # sql="SELECT "  
+   # sql="SELECT "  
 
    return render_template('/admin/adminDashboard.html',x=diction)
 
-
-
-
-"""
-@app.route('/signin',methods=['POST','GET'])  # GET METHOD
+@app.route("/signin",methods=["POST","GET"])  # GET METHOD
 def signin():
-   msg = 'Incorrect username/password!'
-   if request.method == 'POST':
-      if 'patient' in request.form:
-         patientid = request.form['patientID']
-         patientpassword = request.form['patientpassword']
+   if request.method == "POST":
+      #PATIENT ONLY-----------------------------------------------------------------
+      if "patient" in request.form:
+         patientid = request.form["patientID"]
+         patientpassword = request.form["patientpassword"]
          mycursor.execute(
-             'SELECT * FROM patient WHERE patientid = %s AND patientpassword = %s', (patientid, patientpassword,))
+             "SELECT * FROM patient_record WHERE IdPatient_Record = %s AND password = %s", (patientid, patientpassword))
          account = mycursor.fetchone()
          if account:
-            render_template('patient.html')
+            mycursor.execute(
+            "SELECT FirstName FROM patient WHERE patientid = %s AND patientpassword = %s", (patientid, patientpassword))
+            patientname=mycursor.fetchone()
+            session["id"]=patientid
+            return render_template("patienthome.html")
          else: 
-            return msg
-      elif 'doctor' in request.form:
-         doctorid = request.form['doctorID']
-         doctorpassword = request.form['doctorpassword']
+            flash('ID/Password is incorrect','warning')
+            return redirect('/signin')
+      #ENDPATIENT-------------------------------------------------------------------------
+      #DOCTORONLY--------------------------------------------------------------------------
+      elif "doctor" in request.form:
+         doctorid = request.form["doctorID"]
+         doctorpassword = request.form["doctorpassword"]
          mycursor.execute(
-             'SELECT * FROM doctor WHERE doctorid = %s AND doctorpassword = %s', (doctorid, doctorpassword,))
+             "SELECT * FROM doctor WHERE doctorid = %s AND doctorpassword = %s", (doctorid, doctorpassword,))
          account = mycursor.fetchone()
          if account:
-            render_template('doctor.html')
+            mycursor.execute(
+            "SELECT FirstName FROM patient WHERE doctorid = %s AND doctorpassword = %s", (doctorid, doctorpassword,))
+            doctorname=mycursor.fetchone()
+            session["id"]=doctorid
+            return render_template("doctorhome.html")
          else:
-            return msg
-      elif 'nurse' in request.form :
-         nurseid = request.form['nurseID']
-         nursepassword = request.form['nursepassword']
+            flash('ID/Password is incorrect', 'warning')
+            return redirect('/signin')
+      #ENDDOCTOR-------------------------------------------------------------------------------
+      #NURSEONLY-------------------------------------------------------------------------------
+      elif "nurse" in request.form:
+         nurseid = request.form["nurseID"]
+         nursepassword = request.form["nursepassword"]
          mycursor.execute(
-             'SELECT * FROM nurse WHERE nurseid = %s AND nursepassword = %s', (nurseid, nursepassword,))
+             "SELECT * FROM nurse WHERE nurseid = %s AND nursepassword = %s", (nurseid, nursepassword,))
          account = mycursor.fetchone()
          if account:
-            render_template('nurse.html')
+            mycursor.execute(
+            "SELECT FirstName FROM nurse WHERE nurseid = %s AND nursepassword = %s", (nurseid, nursepassword,))
+            nursename=mycursor.fetchone()
+            session["id"]=doctorid
+            return render_template("doctorhome.html")
          else:
-            return msg
-      elif 'admin' in request.form:
-         adminid = request.form['adminID']
-         adminpassword = request.form['adminpassword']
+            flash('ID/Password is incorrect', 'warning')
+            return redirect('/signin')
+      #ENDNURSE-------------------------------------------------------------------------------------
+      #ADMINONLY------------------------------------------------------------------------------------
+      elif "admin" in request.form:
+         adminid = request.form["adminID"]
+         adminpassword = request.form["adminpassword"]
          mycursor.execute(
-             'SELECT * FROM admin WHERE adminid = %s AND adminpassword = %s', (adminid, adminpassword,))
+             "SELECT * FROM admin WHERE adminid = %s AND adminpassword = %s", (adminid, adminpassword,))
          account = mycursor.fetchone()
          if account:
-            render_template('admin.html')
+            mycursor.execute(
+            "SELECT FirstName FROM admin WHERE adminid = %s AND adminpassword = %s", (adminid, adminpassword,))
+            adminname=mycursor.fetchone()
+            session["id"]=adminid
+            return render_template("AdminMain.html")
          else:
-            return msg
-      if 'receptionist' in request.form:
-         receptionistid = request.form['receptionistID']
-         receptionistpassword = request.form['receptionistpassword']
+            flash('ID/Password is incorrect', 'warning')
+            return redirect('/signin')
+      #ADMINEND---------------------------------------------------------------------------------------
+      #RECEPTIONISTSONLY------------------------------------------------------------------------------
+      elif "receptionist" in request.form:
+         receptionistid = request.form["receptionistID"]
+         receptionistpassword = request.form["receptionistpassword"]
          mycursor.execute(
-             'SELECT * FROM receptionist WHERE receptionistid = %s AND receptionistpassword = %s', (receptionistid, receptionistpassword,))
+             "SELECT * FROM receptionist WHERE receptionistid = %s AND receptionistpassword = %s", (receptionistid, receptionistpassword,))
          account = mycursor.fetchone()
          if account:
-            render_template('receptionist1.html')
+            mycursor.execute(
+            "SELECT FirstName FROM receptionist WHERE receptionistid = %s AND receptionistpassword = %s", (receptionistid, receptionistpassword,))
+            adminname=mycursor.fetchone()
+            session["id"]=adminid
+            return render_template("receptionist1.html")
          else:
-            return msg
-      # If account exists in accounts table in out database
-      #if account:
-         # Create session data, we can access this data in other routes
-         #session['loggedin'] = True
-         #session['id'] = account['id']
-         #session['username'] = account['username']
-         # Redirect to home page
-         #return 'Logged in successfully!'
-      #else:    
-         # Account doesnt exist or username/password incorrect
-         #msg = 'Incorrect username/password!'
-   else:
-      return render_template('signin.html')
+            flash('ID/Password is incorrect', 'warning')
+            return redirect('/signin')
+      #RECEPTIONISTEND-------------------------------------------------------------------------------
+   return render_template('signin.html')
 
-@app.route('/contactus')  # GET METHOD
+@app.route("/contactus",methods=["POST","GET"])  # GET METHOD
 def contactus():
-   return render_template('contactus.html')
+   if request.method == "POST":
+      result={}
+      result['firstname'] = request.form["FirstName"]
+      result['lastname'] = request.form["LastName"]
+      result['email'] = request.form["email"]
+      result['complaint']= request.form["complain"]
+      sendmessage(result)
+      flash('Email is successfully sent','success')
+      return render_template("contactus.html")
+   return render_template("contactus.html")
 
-"""
 
+@app.route("/AdminMain")
+def AdminMain():
+   return render_template("AdminMain.html")
 
+@app.route("/logout")
+def LogOut():
+   session.pop("id",None)
+   redirect("/")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
    app.run(debug=True)
